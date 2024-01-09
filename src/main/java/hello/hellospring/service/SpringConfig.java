db7b6df13c9fd9419b22a3ce3aede254196cedb2
seyeon321 <seyeon321>
2024-01-09 21:01:51 +0900

    스프링데이터jpa

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,35 +1,21 @@
 package hello.hellospring.service;
-
-import hello.hellospring.repository.*;
-import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import javax.swing.*;
-
+        import hello.hellospring.repository.*;
+        import hello.hellospring.service.MemberService;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
-//    private DataSource dataSource;
-//    //di 스프링부트가 db정보보고 datasource를 만들어주고 주입해줌
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource=dataSource;
-//    }
-    private EntityManager em;
-    public SpringConfig(EntityManager em){
-        this.em=em;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-    // memberService가 spring bean에 등록됨
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
